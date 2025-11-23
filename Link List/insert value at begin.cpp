4fe5e7c549acf5c6de28af00f2181e362a0fcd3c
Md. Habibur Rahman <156560375+Habibur-02@users.noreply.github.com>
2025-11-24 02:40:31 +0600

    Create insert value at begin.cpp

---
+++ Link List/insert value at begin.cpp
@@ -0,0 +1,108 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node{
+    int data;
+    struct node *next;
+    
+};
+struct node* makelink(int v[])
+{
+    struct node* head=new node();
+    // cout<<v[0];
+    struct node* temp= new node();
+    head->data=v[0];
+    head->next=NULL;
+    temp=head;
+    
+    for(int i=1;i<5;i++)
+    {
+        
+        struct node* p=new node();
+        p->data=v[i];
+        p->next=NULL;
+        temp->next=p;
+        temp=temp->next;
+        // cout<<temp->data<<" ";
+    }
+    
+    // head=temp;
+    struct node* k=new node();
+    k=head;
+    
+    // while(k->next!=NULL)
+    // {
+    //     cout<<k->data<<" ";
+    //     k=k->next;
+        
+    // }
+    // cout<<k->data;
+    return head;
+    
+}
+
+struct node* insertValBegin(struct node*head,int val)
+{
+    cout<<'\n';
+    cout<<head->data;
+    
+    struct node* temp=new node();
+    temp=head;
+    struct node* x=new node;
+    x->data=val;
+    x->next= temp;
+    head=x;
+    cout<<'\n';
+    while(x->next != NULL)
+    {
+        cout<<x->data<<" ";
+        x=x->next;
+        
+    }
+    cout<< x->data;
+    
+    
+    return head;
+}
+int main()
+{
+    // std::cout<<"Hello World";
+    
+    struct node *head=new node();
+    head->data= 10;
+    head->next=NULL;
+    // cout<<head->data<<'\n';
+    vector<int>v(5);
+    int v1[5]={2,6,1,2,3};
+    
+    // struct node* head=new node();
+    cout<<'\n';
+    
+    head=makelink(v1);
+    cout<<head<<'\n';
+    
+    struct node* insert=insertValBegin(head,10);
+    cout<<"----------------"<<'\n';
+    
+  while(insert->next != NULL)
+    {
+        cout<<insert->data<<" ";
+        insert=insert->next;
+        
+    }
+    cout<< insert->data;
+    
+    
+    
+    
+
+    return 0;
+}
