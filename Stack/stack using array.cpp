83a826a6ecf1c56ecfb5bc9e4fc70bd3de02d472
Md. Habibur Rahman <156560375+Habibur-02@users.noreply.github.com>
2025-12-17 22:30:46 +0600

    Create stack using array.cpp

---
+++ Stack/stack using array.cpp
@@ -0,0 +1,80 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+#define len 3 
+int st[len];
+int top=-1;
+
+void push(int x)
+{
+    if(top<len-1)
+    {
+        top=top+1;
+        st[top]=x;
+        cout<<"Successfully added"<<'\n';
+        
+    }
+    else
+    {
+        cout<<"Execption! No spaces"<<'\n';
+    }
+}
+void pop()
+{
+    // return -1;
+    if(top>=0)
+    {
+        cout<<"Current value "<<st[top]<<'\n';
+        top--;
+        cout<<"Remove Suceesfully"<<'\n';
+        
+    }
+    else
+    {
+        cout<<"There are no value in array"<<'\n';
+    }
+}
+int peek()
+{
+    // return -1;
+    if(top>=0)
+    {
+        return st[top];
+        
+    }
+    else
+    {
+        return -1;
+    }
+}
+int main()
+{
+   push(10);
+   push(20);
+   push(30);
+   push(40);
+   cout<<peek();
+   pop();
+      cout<<peek()<<'\n';
+
+   
+   pop();
+      cout<<peek()<<'\n';
+
+   pop();
+      cout<<peek()<<'\n';
+
+   pop();
+      cout<<peek()<<'\n';
+
+   
+    return 0;
+}
