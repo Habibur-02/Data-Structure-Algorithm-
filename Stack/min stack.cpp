c87e13b97a1a4a61255d37171c57a0adf868122c
Md. Habibur Rahman <156560375+Habibur-02@users.noreply.github.com>
2025-12-21 23:50:18 +0600

    Update min stack.cpp

--- Stack/min stack.cpp
+++ Stack/min stack.cpp
@@ -1,12 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+stack<pair<int,int>>st;
+int minvalue=INT_MAX;
 void minstack2n(vector<int>v)
 {
     for(auto kk: v) cout<<kk<<" ";
     // reverse(v.begin(),v.end());
-    stack<pair<int,int>>st;
-    int minvalue=INT_MAX;
+    
+    
     cout<<'\n';
     for(auto k: v)
     {
@@ -22,8 +23,45 @@ void minstack2n(vector<int>v)
             minvalue=min(minvalue, k);
             cout<<" Min value"<<minvalue<<'\n';
         }
+       
     }
-    cout<<'\n';
+}
+
+
+int push(int val)
+{
+    if(st.empty())
+    {
+        minvalue=min(val,minvalue);
+        st.push({val, minvalue});
+        
+    }
+    else
+    {
+        // int top=st.top();
+        minvalue=min(val,st.top().second);
+        st.push({val, minvalue});
+        
+    }
+    return 0;
+}
+    
+void pop()
+{
+    int min_val=st.top().second;
+    st.pop();
+    
+    
+    
+}
+
+    int top()
+    {
+        return st.top().second;
+    }
+    
+void printstack()
+{
     stack<pair<int,int>>stt=st;
     while(!stt.empty())
     {
@@ -35,8 +73,28 @@ void minstack2n(vector<int>v)
 int main()
 {
  cout<<"Trying min stack"<<'\n';
-vector<int>v={10,2,3,7,9};
+vector<int>v={100,20,30,70,90};
 
 minstack2n(v);
+
+push(10);
+push(90);
+push(9);
+printstack();
+
+cout<<"-------------------"<<'\n';
+
+pop();
+printstack();
+cout<<"-------------------"<<'\n';
+
+pop();
+printstack();
+cout<<"-------------------"<<'\n';
+pop();
+
+printstack();
+
+pop();
  return 0;
 }
