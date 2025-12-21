8e55ac6c7ce5240cc87f22e542b72b2fd7963b40
Md. Habibur Rahman <156560375+Habibur-02@users.noreply.github.com>
2025-12-21 23:18:10 +0600

    Update min stack.cpp

--- Stack/min stack.cpp
+++ Stack/min stack.cpp
@@ -1,6 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(
+
+void minstack2n(vector<int>v)
+{
+    for(auto kk: v) cout<<kk<<" ";
+    // reverse(v.begin(),v.end());
+    stack<pair<int,int>>st;
+    int minvalue=INT_MAX;
+    cout<<'\n';
+    for(auto k: v)
+    {
+        if(st.empty())
+        {
+            st.push({k,min(k,minvalue)});
+            minvalue=min(k,minvalue);
+            cout<<" Min value"<<minvalue<<'\n';
+        }
+        else
+        {
+            st.push({k, min(minvalue, k)});
+            minvalue=min(minvalue, k);
+            cout<<" Min value"<<minvalue<<'\n';
+        }
+    }
+    cout<<'\n';
+    stack<pair<int,int>>stt=st;
+    while(!stt.empty())
+    {
+        cout<<stt.top().first<<" "<<stt.top().second<<'\n';
+        stt.pop();
+    }
+}
+
+int main()
 {
  cout<<"Trying min stack"<<'\n';
+vector<int>v={10,2,3,7,9};
+
+minstack2n(v);
+ return 0;
 }
