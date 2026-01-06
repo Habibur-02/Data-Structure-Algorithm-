af5bd600b31a9d818bb5828e7c3b96910948d919
Md. Habibur Rahman <156560375+Habibur-02@users.noreply.github.com>
2026-01-06 22:58:58 +0600

    Create simple.cpp

---
+++ simple.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node {
+    int data;
+    struct node* next;
+};
+int main()
+{
+    cout<<"aasif";
+    node* head= new node();
+    head->data= 10;
+    head->next= NULL;
+    
+    cout<<head->data;
+    
+}
